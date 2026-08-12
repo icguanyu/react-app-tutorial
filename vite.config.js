3cfb69660af6606205c92015104758de6faaf65d
Nick <icguanyu@gmail.com>
2026-08-12 17:18:37 +0800

    feat: 新增 GitHub Pages 部署工作流程及 404 頁面處理
    - 新增 deploy.yml 檔案以設定 GitHub Pages 的自動部署流程
    - 更新 index.html 以支援 BrowserRouter 的路由處理
    - 新增 404.html 檔案以處理未知路由的情況

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import { fileURLToPath, URL } from 'url'
 
 export default defineConfig({
+  base: '/react-app-tutorial/',
   plugins: [react()],
   resolve: {
     alias: {
